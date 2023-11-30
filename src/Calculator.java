aa7b7feabc94658f5f172a636791ad186e214061
Lastman89 <alexeygr89@gmail.com>
2023-11-30 19:03:00 +0300

    На проверку с учетом замечаний

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,7 +8,9 @@ public class Calculator {
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
 
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> (y == 0) ? 0 : x / y;
+
+
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,7 @@ public class Main {
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        if (calc.check.test(a) || calc.check.test(b)) {
-            throw new Exception("Деление на ноль");
-        }
+
         int c = calc.devide.apply(a, b);
 
         calc.println.accept(c);

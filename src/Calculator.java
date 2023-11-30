4288fe1ea417c2dea931a123b0a107a5324bffc2
Lastman89 <alexeygr89@gmail.com>
2023-11-30 19:14:00 +0300

    На проверку с учетом замечаний

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,8 +10,6 @@ public class Calculator {
 
     BinaryOperator<Integer> devide = (x, y) -> (y == 0) ? 0 : x / y;
 
-
-
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
 
